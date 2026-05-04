70170f858d87e6cfbd9cde5e12330bd233b8f826
Mateusz <261795@edu.p.lodz.pl>
2026-05-04 19:25:16 +0200

    added everything

--- main.cpp
+++ main.cpp
@@ -1,26 +1,50 @@
 #include <iostream>
-#iclude <cstring>
+#include <cstring>
 
-void usage(char *progname) {
-    printf("Usage: %s number1 operation number2\n", progname);
+void usage(char *programName) {
+    printf("Usage: %s number1 operation number\n", programName);
     printf("Available operations:\n");
-    printf("\add\n");
-    printf("\sub\n");
-    printf("\mul\n");
-    printf("\div\n");
-    printf("\mod\n");
-    printf("\ave\n");
+    printf("\tadd\n");
+    printf("\n");
 }
-int main(int argc, char* argv[]) {
-    int number1=0;
-    int number2=0;
-    int result=0;
-    printf("%d",number1);
 
-    if (!strcmp("divide", argv[2])) {
-        result = number1 / number2;
-        printf("%d / %d = %d\n", number1, number2, result);
-        return 0;
+int main(int argc, char* argv[]) {
+
+    int number1 = 0;
+    int number2 = 0;
+    int result = 0;
+
+    if (argc == 4) {
+        number1 = atoi(argv[1]);
+        number2 = atoi(argv[3]);
+
+        if (!strcmp("add", argv[2])) {
+            result = number1 + number2;
+            printf("%d + %d = %d\n", number1, number2, result);
+            return 0;
+        }
+
+        if (!strcmp("subtract", argv[2])) {
+            result = number1 - number2;
+            printf("%d - %d = %d\n", number1, number2, result);
+            return 0;
+        }
+
+        if (!strcmp("multiply", argv[2])) {
+            result = number1 * number2;
+            printf("%d * %d = %d\n", number1, number2, result);
+            return 0;
+        }
+
+        if (!strcmp("divide", argv[2])) {
+            result = number1 / number2;
+            printf("%d / %d = %d\n", number1, number2, result);
+            return 0;
+        }
+
     }
 
+    usage(argv[0]);
+
+    return 0;
 }
